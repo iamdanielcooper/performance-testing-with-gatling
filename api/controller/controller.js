2acb2fe0fed4c8a6b499f4e61d157e9d6a8bdb96
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-12-15 21:42:46 +0000

    Add caching for users

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -1,14 +1,33 @@
 const Users = require('../models/Users');
+const cache = require('../cache/cache');
 
 const getAllUsers = async (req, res) => {
-    const results = await Users.getAll;
-    console.log(results);
-    res.send(results);
+    console.info('Retrieving user data from cache...');
+    const users = cache.get('users');
+
+    let userData;
+    if (!users) {
+        console.info('User data absent from cache...');
+        userData = await Users.getAll;
+
+        console.info('Adding user data to cache...');
+        success = cache.set('users', userData, 10000);
+
+        const cacheStatus = success
+            ? 'Successfully added to cache'
+            : 'Error adding to cache.';
+        console.info(cacheStatus);
+    } else {
+        userData = users;
+    }
+    res.send(userData);
 };
 
 const addUser = async (req, res) => {
+    console.log(req);
+
     const result = await Users.create({ id: '0', name: 'Dan' });
-    console.log(result);
+    cache.del('users');
     res.send(result);
 };
 
